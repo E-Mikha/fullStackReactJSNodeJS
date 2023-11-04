cb9475b90aed369a185be845baa0d2bfbee9f5c2
Mikhail Elanov <Elanov1991@Gmail.com>
2023-11-04 16:07:32 +0700

    Add auth

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.post("/auth/login", (req, res) => {
+  console.log(req.body);
+
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+      fullName: "Misha Ela",
+    },
+    "secret123"
+  );
+  res.json({
+    success: true,
+  });
 });
 
 app.listen(4444, (err) => {
