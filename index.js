fdf3d575f7dc5dd56b69437a5c22a47f3105a416
Mikhail Elanov <Elanov1991@Gmail.com>
2023-11-27 12:57:27 +0700

    Add mongoose database

--- index.js
+++ index.js
@@ -1,32 +1,25 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:123456abcd@cluster0.4wsoaoy.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("DB ok");
+  })
+  .catch(() => {
+    console.log("DB error", err);
+  });
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.get("/", (req, res) => {});
 
-app.post("/auth/login", (req, res) => {
-  console.log(req.body);
-
-  if (req.body.email === "test@test.ru") {
-    const token = jwt.sign(
-      {
-        email: req.body.email,
-        fullName: "Misha Ela",
-      },
-      "secret123"
-    );
-  }
-
-  res.json({
-    success: true,
-    token,
-  });
-});
+app.post("/auth/register", (req, res) => {});
 
 app.listen(4444, (err) => {
   if (err) {
