49eeabe9b5e9734863388c2e68e2c243cb5538ac
Mikhail Elanov <Elanov1991@Gmail.com>
2023-12-01 12:17:05 +0700

    add regestration for new users

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 import { validationResult } from "express-validator";
 
 import { registerValidation } from "./validations/auth.js";
+
+import UserModel from "./models/User.js";
 
 mongoose
   .connect(
@@ -22,15 +25,26 @@ app.use(express.json());
 
 app.get("/", (req, res) => {});
 
-app.post("/auth/register", registerValidation, (req, res) => {
+app.post("/auth/register", registerValidation, async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json(errors.array());
   }
 
-  res.json({
-    success: true,
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const doc = new UserModel({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl,
+    passwordHash: req.body.passwordHash,
   });
+
+  const user = await doc.save();
+
+  res.json(user);
 });
 
 app.listen(4444, (err) => {
