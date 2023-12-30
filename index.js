7dfd808bc30e907269a3df2ebc03342be68b6be4
Mikhail Elanov <Elanov1991@Gmail.com>
2023-12-30 12:00:51 +0700

    Small update

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ app.post("/auth/register", registerValidation, UserController.register);
 app.get("/auth/me", checkAuth, UserController.getMe);
 
 app.get("/posts", PostController.getAll);
-// app.get("/posts/:id", PostController.getOne);
+app.get("/posts/:id", PostController.getOne);
 app.post("/posts", checkAuth, postCreateValidation, PostController.create);
-// app.delete("/posts", PostController.remove);
+// app.delete("/posts/:id", PostController.remove);
 // app.patch("/posts", PostController.update);
 
 app.listen(4444, (err) => {
