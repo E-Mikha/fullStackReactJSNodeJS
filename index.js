31b30e118e12b6823524a5ca770d53457ea2b5c4
Mikhail Elanov <Elanov1991@Gmail.com>
2024-01-08 11:30:14 +0700

    Add update controlers

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ app.get("/auth/me", checkAuth, UserController.getMe);
 app.get("/posts", PostController.getAll);
 app.get("/posts/:id", PostController.getOne);
 app.post("/posts", checkAuth, postCreateValidation, PostController.create);
-// app.delete("/posts/:id", PostController.remove);
-// app.patch("/posts", PostController.update);
+app.delete("/posts/:id", checkAuth, PostController.remove);
+app.patch("/posts/:id", checkAuth, PostController.update);
 
 app.listen(4444, (err) => {
   if (err) {
